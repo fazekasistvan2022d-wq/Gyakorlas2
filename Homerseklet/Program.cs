c8ec1e9dfbc39efd5a1e6db3f6bd2fe7205cbb3e
fazekasistvan2022d-wq <fazekas.istvan.2022d@telekimezotur.hu>
2026-09-17 08:47:34 +0200

    2. feladat

--- Homerseklet/Program.cs
+++ Homerseklet/Program.cs
@@ -10,7 +10,11 @@ namespace Homerseklet
     {
         static void Main(string[] args)
         {
-            
+            HomersekletAtvalto homer1 = new HomersekletAtvalto();
+            homer1.Celsius = 28;
+
+            Console.WriteLine(homer1.ToFahrenheit()+" Példányosított");
+            Console.WriteLine(HomersekletAtvalto.CelsiusToFahrenheit(28)+" Static");
         }
     }
 }
